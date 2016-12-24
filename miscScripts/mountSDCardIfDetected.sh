d10ac71c7628eeb395494c697d57869786ebf4bc
Izaac Kirscht <izaac.kirscht@gmail.com>
2016-12-24 09:44:30 -0600

    Update mountSDCardIfDetected.sh

--- miscScripts/mountSDCardIfDetected.sh
+++ miscScripts/mountSDCardIfDetected.sh
@@ -1,1 +1,1 @@
-if fdisk -l | grep mmcblk1p1 ; then echo "Drive present..."; else sudo mount /dev/mmcblk1p1 /home/stuxnet/samba/SDCard/; fi
+if fdisk -l | grep mmcblk1p1 ; then sudo mount /dev/mmcblk1p1 /home/stuxnet/samba/SDCard/; else echo "Drive Present..." fi
